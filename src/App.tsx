bf0b595256cb6f53d782da8069f875f9f251008f
wangyaming-08 <127426320+wangyaming-08@users.noreply.github.com>
2023-11-01 10:55:43 +0800

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+
+
+  return (
+    <div className="App">
+      顶级组件
+    </div>
+  )
+}
+
+export default App
---
+++ src/main.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import 'reset-css'
+import  '@/assets/styles/global.scss'
+
+import App from './App.tsx'
+
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
